30c3f5f5801282a3ba85d5941300a36927b24572
Luiz Lopes <luizlopes12@outlook.com>
2022-02-10 12:44:49 -0300

    ajustes e pegando familiaridade com a api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { useState, useEffect } from 'react';
+import API from '../services/axios/API'
 
 function App() {
+  useEffect(() =>{
+    API.get('/indicadores')
+    .then(response =>{
+      console.log('Deu certo\n', response)
+    })
+    .catch(error =>{
+      console.log('Deu erro\n', error)
+    })
+  },[])
   return (
     <div></div>
   );
