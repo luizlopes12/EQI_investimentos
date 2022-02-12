e689e239352a25c3bfba24f38e84ef3d74e1735d
Luiz Lopes <luizlopes12@outlook.com>
2022-02-12 13:32:19 -0300

    criando o container e organizando pastas

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
     })
   },[])
   return (
-    <div>
+    <>
 
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Container.css'
+function Container() {
+    return (
+        <>
+            <header>
+                <h1>Simulador de Investimentos</h1>
+            </header>
+            <main>
+            
+            </main>
+        </>
+
+    );
+}
+
+export default Container;
