bb3ca63c824dd0d673d59c67fc0861fb491015a2
Luiz Lopes <luizlopes12@outlook.com>
2022-02-14 15:21:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import {API} from './services/axios/API'
 
 function App() {
   const [indicadores, setIndicadores] = useState(false)
-  const [simulacoes, setSimulacoes] = useState(false)
 
   useEffect(() =>{
     API.get('/indicadores')
     .then(response =>{
-      setIndicadores(response.data)
-      console.log('Deu certo\n', response.data)
-    })
-    .catch(error =>{
-      console.log('Deu erro\n', error)
-    })
-    API.get('/simulacoes')
-    .then(response =>{
-      setSimulacoes(response.data)
+      indicadores != false && setIndicadores(response.data)
       console.log('Deu certo\n', response.data)
     })
     .catch(error =>{
@@ -27,7 +18,7 @@ function App() {
   },[])
   return (
     <>
-    <Container indicadores={indicadores != false && indicadores}/>
+    <Container indicadores={indicadores}/>
     </>
   );
 }
